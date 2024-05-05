dcb1cfea693e19fdfdd94df2462beaee57ccef6c
OPOLO <misha90638@gmail.com>
2024-05-05 14:35:40 +0300

    апдейт для хука

--- app.py
+++ app.py
@@ -62,5 +62,6 @@ async def github_webhook(request: Request):
 app.mount("/", StaticFiles(directory="www", html=True), name="static")
 
 
+
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=7742, reload=True)
